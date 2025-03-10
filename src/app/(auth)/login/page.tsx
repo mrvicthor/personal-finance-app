0d485257a422ecb9614bebb9c03db3dd672feca0
Victor Eleanya <victoreleanya89@gmail.com>
2025-03-10 15:54:55 +0000

    fix: amend css

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import LoginForm from "./form";
 const Page = () => {
   return (
     <div className="flex items-center justify-center md:h-screen">
-      <div className="bg-white rounded-lg w-[21.4375rem] sm:min-w-[35rem]  py-8 px-8">
+      <div className="bg-white rounded-lg w-[21.4375rem] sm:min-w-[35rem] py-8 px-4">
         <h1 className="text-[2rem] font-bold capitalize text-[#201F24]">
           login
         </h1>
