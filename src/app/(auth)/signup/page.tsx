cb561de3e9a52cefa73e677d137bd3e60ae4d573
Victor Eleanya <victoreleanya89@gmail.com>
2025-03-10 16:01:29 +0000

    fix(form.tsx): amend styling

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -3,8 +3,8 @@ import SignupForm from "./form";
 
 const Page = () => {
   return (
-    <div className="flex items-center justify-center md:h-screen">
-      <div className="bg-white rounded-lg w-[21.4375rem] sm:min-w-[35rem]  py-8 px-4">
+    <div className="flex items-center justify-center md:h-screen px-4 sm:px-0">
+      <div className="bg-white rounded-lg w-full sm:max-w-[35rem]  py-8 px-5 sm:px-8">
         <h1 className="text-[2rem] font-bold capitalize text-[#201F24]">
           sign up
         </h1>
