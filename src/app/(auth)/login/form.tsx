235cd4cb54ec5027e018835eb7d8b7a1f6d46c7e
Victor Eleanya <victoreleanya89@gmail.com>
2025-03-10 15:29:50 +0000

    feat(login.tsx): designed login page for mobile, tablet and desktop device

--- src/app/(auth)/login/form.tsx
+++ src/app/(auth)/login/form.tsx
@@ -3,19 +3,10 @@ import Image from "next/image";
 import eyeIcon from "../../../../public/assets/images/icon-show-password.svg";
 import Link from "next/link";
 
-const SignupForm = () => {
+const LoginForm = () => {
   return (
-    <form className="mt-8">
+    <form className="mt-8 ">
       <div className="space-y-4">
-        <div className="flex flex-col gap-1">
-          <label className="capitalize text-[#696868] text-xs font-bold">
-            name
-          </label>
-          <input
-            className="border-[#98908B] border rounded-lg h-[2.8125rem]"
-            type="text"
-          />
-        </div>
         <div className="flex flex-col gap-1">
           <label className="capitalize text-[#696868] text-xs font-bold">
             email
@@ -27,7 +18,7 @@ const SignupForm = () => {
         </div>
         <div className="flex flex-col gap-1">
           <label className="capitalize text-[#696868] text-xs font-bold">
-            create password
+            password
           </label>
           <div className="relative w-full">
             <input
@@ -42,25 +33,22 @@ const SignupForm = () => {
               className="absolute right-2 top-1/2 -translate-y-1/2"
             />
           </div>
-          <p className="text-right text-xs text-[#696868]">
-            Password must be at least 8 characters
-          </p>
         </div>
       </div>
       <button
         type="submit"
         className="mt-8 bg-[#201F24] text-white w-full h-[3.3125rem] rounded-lg text-sm font-bold capitalize cursor-pointer"
       >
-        create account
+        login
       </button>
       <p className="text-[#696868] text-xs mt-4 text-center">
-        Already have an account?{" "}
-        <Link href="/login" className="text-[#201F24] font-bold">
-          Login
+        Need to create an account?{" "}
+        <Link href="/signup" className="text-[#201F24] font-bold">
+          Sign Up
         </Link>
       </p>
     </form>
   );
 };
 
-export default SignupForm;
+export default LoginForm;
