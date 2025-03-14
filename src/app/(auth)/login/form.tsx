ea75a23f1545db57cf3e960aa6f40674118f3e62
Victor Eleanya <victoreleanya89@gmail.com>
2025-03-14 19:30:45 +0000

    fix(loginform.tsx): added use client to fix errors

--- src/app/(auth)/login/form.tsx
+++ src/app/(auth)/login/form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useActionState } from "react";
 import Image from "next/image";
 import eyeIcon from "../../../../public/assets/images/icon-show-password.svg";
