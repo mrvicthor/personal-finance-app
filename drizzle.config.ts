c1260cffe9d978af529db74ddb1113200e0b92f3
Victor Eleanya <victoreleanya89@gmail.com>
2025-09-08 22:59:46 +0100

    fix(drizzle.config.ts): update file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle/migrations",
-  dialect: "postgresql",
+  driver: "pg",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
