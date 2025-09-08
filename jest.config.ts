d7eeb2b36264d481d092b9a7dd9755c17c402bba
Victor Eleanya <victoreleanya89@gmail.com>
2025-09-08 23:04:18 +0100

    update(jest.config.ts): update jest file

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,14 @@
  * https://jestjs.io/docs/configuration
  */
 import nextJest from "next/jest.js";
-import type { Config } from "jest";
+import type { Config } from "@jest/types";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-const config: Config = {
+const config: Config.InitialOptions = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
