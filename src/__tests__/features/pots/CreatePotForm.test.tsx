676928e6f60d050a68397dadfabeb711d5b800f2
Victor Eleanya <victoreleanya89@gmail.com>
2025-09-11 17:58:04 +0100

    test(CreatePotForm.test.tsx): added test case for Form interaction

--- src/__tests__/features/pots/CreatePotForm.test.tsx
+++ src/__tests__/features/pots/CreatePotForm.test.tsx
@@ -1,6 +1,7 @@
 import { getPots } from "@/features/pots/actions/pots";
 import CreatePotForm from "@/features/pots/components/forms/CreatePotForm";
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("lucide-react", () => {
   return new Proxy(
@@ -41,5 +42,25 @@ describe("Create Pot Form", () => {
     expect(getPots).toHaveBeenCalled();
   });
 
-  test("");
+  describe("Form Interaction", () => {
+    test("should allow user to type in the fields", async () => {
+      render(<CreatePotForm />);
+      const user = userEvent.setup();
+      const form = screen.queryByTestId("add-pot-form");
+      const potNameInput = screen.getByLabelText("pot name");
+      const targetInput = screen.getByLabelText("target");
+      const colorTagInput = screen.getByTestId("select-theme");
+      const trigger = screen.getByTestId("select-trigger-theme");
+
+      await user.type(potNameInput, "Vacation");
+      await user.type(targetInput, "1000");
+      await user.click(trigger);
+      expect(form).toBeInTheDocument();
+      expect(potNameInput).toHaveValue("Vacation");
+      expect(targetInput).toHaveValue("1000");
+      await waitFor(() => {
+        expect(screen.getByTestId("select-options-theme")).toBeInTheDocument();
+      });
+    });
+  });
 });
