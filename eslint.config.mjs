01559cd1f8c32b298579dca3de3225e152ebdaa9
Victor Eleanya <victoreleanya89@gmail.com>
2025-10-01 18:27:20 +0100

    fix(eslint.config.mjs): fix eslint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,26 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import next from "eslint-config-next";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [{
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
-}, ...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+  ...next(["core-web-vitals", "typescript"]),
+];
 
 export default eslintConfig;
